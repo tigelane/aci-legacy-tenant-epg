03e7cadae2890bbb39f75250ce21644839a811b6
tigelane <tige@ignw.io>
2020-09-17 21:38:22 -0700

    doh

--- network_192.168.2_V210.tf
+++ network_192.168.2_V210.tf
@@ -1,4 +1,4 @@
-resource "aci_bridge_domain" "bridge_domain_192_168_2_V210" {
+resource "aci_bridge_domain" "bridge_domain_192_168_210_V210" {
   tenant_dn               = "${aci_tenant.tenant.id}"
   relation_fv_rs_ctx      = "${aci_vrf.vrf.name}"
   name                    = "192.168.210_V210"
