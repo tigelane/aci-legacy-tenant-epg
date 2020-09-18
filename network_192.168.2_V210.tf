35fdff63badd9cb3e4700f79f0b0a9b1976ff7cd
tigelane <tige@ignw.io>
2020-09-17 21:37:15 -0700

    Add V210 network

---
+++ network_192.168.2_V210.tf
@@ -0,0 +1,18 @@
+resource "aci_bridge_domain" "bridge_domain_192_168_2_V210" {
+  tenant_dn               = "${aci_tenant.tenant.id}"
+  relation_fv_rs_ctx      = "${aci_vrf.vrf.name}"
+  name                    = "192.168.210_V210"
+}
+
+resource "aci_subnet" "subnet_192_168_210_V210" {
+  bridge_domain_dn        = "${aci_bridge_domain.bridge_domain_192_168_210_V210.id}"
+  ip                      = "192.168.210.1/24"
+  scope                   = "private"
+}
+
+resource "aci_application_epg" "epg_192_168_210_V210" {
+  application_profile_dn  = "${aci_application_profile.app_profile.id}"
+  name                    = "${aci_bridge_domain.bridge_domain_192_168_210_V210.name}"
+  description             = "I was built for the some servers!"
+  relation_fv_rs_bd       = "${aci_bridge_domain.bridge_domain_192_168_210_V210.name}"
+}
