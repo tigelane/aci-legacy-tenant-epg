719d470fd68c18f0a4f4834b755adf188af07871
tigelane <tige@ignw.io>
2020-09-18 08:34:31 -0700

    missed one

--- network_192.168.2_V200.tf
+++ network_192.168.2_V200.tf
@@ -13,6 +13,6 @@ resource "aci_subnet" "subnet_192_168_2_V200" {
 resource "aci_application_epg" "epg_192_168_2_V200" {
   application_profile_dn  = "${aci_application_profile.app_profile.id}"
   name                    = "${aci_bridge_domain.bridge_domain_192_168_2_V200.name}"
-  description             = "I was built for the some servers!"
-  relation_fv_rs_bd       = "${aci_bridge_domain.bridge_domain_192_168_2_V200.name}"
+  description             = "I was built for servers!"
+  relation_fv_rs_bd       = "${aci_bridge_domain.bridge_domain_192_168_2_V200.id}"
 }
